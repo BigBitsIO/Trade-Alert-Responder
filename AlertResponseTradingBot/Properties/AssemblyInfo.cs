04e19ae085bfaf83f08574e442437623f2d12735
David-BigBits <David@BigBits.io>
2020-05-13 11:20:47 -0400

    I have the installer project working now, includes perhaps too many files.  Better safe than sorry.

--- AlertResponseTradingBot/Properties/AssemblyInfo.cs
+++ AlertResponseTradingBot/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TradeAlertResponder")]
+[assembly: AssemblyTitle("Trade Alert Responder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TradeAlertResponder")]
+[assembly: AssemblyProduct("Trade Alert Responder")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
