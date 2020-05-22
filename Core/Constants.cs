bff7f34ae9682e8b73bf02a4cd1b938618fed177
David-BigBits <David@BigBits.io>
2020-05-22 13:46:38 -0400

    Version 1.0.1 Release

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -34,7 +34,7 @@ namespace Core
         public static string AlertScanPluginFolder = Constants.AppFolder(Constants.AppDirectory.AlertScanPlugins);
         public static string AlertActionPluginFolder = Constants.AppFolder(Constants.AppDirectory.AlertActionPlugins);
         public static string LogsFolder = Constants.AppFolder(Constants.AppDirectory.Logs);
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public static string AppFolder(AppDirectory Directory)
         {
